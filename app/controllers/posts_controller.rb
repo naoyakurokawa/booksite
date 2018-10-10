d3b6f6298e0a8c288a07a775d604262e5a1ab586
Naoya Kurokawa <kurokawa14@gmail.com>
2018-10-10 20:07:05 +0900

    Image function correction

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,13 +19,10 @@ class PostsController < ApplicationController
       article_title: params[:article_title],
       article_body: params[:article_body])
 
-    if params[:image_book]
-      @post.image_book = "#{@post.id}.png"
-      image = params[:image_book]
-      File.binwrite("public/book_images/#{@post.image_book}",image_book.read)
-    end
-
     if @post.save
+      # image = params[:image_book]
+      # File.binwrite("public/book_images/#{@post.image_book}",params[:image].read)
+
      redirect_to("/posts/index")
    else
      render("posts/new")
@@ -42,16 +39,17 @@ class PostsController < ApplicationController
       name_of_publisher: params[:name_of_publisher],
       release_date: params[:release_date],
       article_title: params[:article_title],
-      article_body: params[:article_body],
-      image_book: params[:image])
-
-      if params[:image]
-        @post.image_book = "#{@post.id}.png"
-        image = params[:image]
-        File.binwrite("public/book_images/#{@post.image_book}",image.read)
-      end
+      article_body: params[:article_body])
 
       if @post.save
+        @post.reload
+        logger.debug("------------------------")
+        logger.debug(@post.id)
+          @post.image_book = "#{@post.id}.png"
+          @post.save
+
+          image = params[:image]
+          File.binwrite("public/book_images/#{@post.image_book}",image.read)
        redirect_to("/posts/index")
      else
        render("posts/edit")
